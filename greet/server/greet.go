c567746059ab34ff626cadb89a366b9912da6422
anuj <ganuj0302@gmail.com>
2024-07-16 01:29:32 +0530

    unary api in grpc
    unary api in grpc

---
+++ greet/server/greet.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/stepup99/go-grpc/greet/proto"
+)
+
+func (*Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
+	log.Printf("Greet was invoked with %v\n", in)
+	return &pb.GreetResponse{Result: "Hello adasdasd " + in.FirstName}, nil
+}
