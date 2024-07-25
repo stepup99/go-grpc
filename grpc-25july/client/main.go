d168d12ccb3856f591025884ac941bf2bc798975
anuj <ganuj0302@gmail.com>
2024-07-25 15:59:08 +0530

    second commit

--- grpc-25july/client/main.go
+++ grpc-25july/client/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
-	"grpc-25july/proto"
 	"log"
 	"time"
+
+	proto "grpc-25july/proto"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,29 +16,25 @@ const (
 )
 
 func main() {
-	conn, err := grpc.NewClient("locahost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
-
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("did not connect %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
-
 	defer conn.Close()
 
 	client := proto.NewMessageExampleClient(conn)
-
 	callSayHello(client)
-
 }
 
 func callSayHello(client proto.MessageExampleClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
 	res, err := client.ServerReply(ctx, &proto.HelloRequest{
 		Msg: "this is aparana",
 	})
-
 	if err != nil {
-		log.Fatalf("could not greet %v", err)
+		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("message %v", res.Msg)
+	log.Printf("message: %v", res.Msg)
 }
--- grpc-25july/server/main.go
+++ grpc-25july/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
-	proto "grpc-25july/proto"
+	"fmt"
 	"log"
 	"net"
+
+	proto "grpc-25july/proto"
 
 	"google.golang.org/grpc"
 )
@@ -13,23 +15,25 @@ type server struct {
 	proto.UnimplementedMessageExampleServer
 }
 
+func (s *server) ServerReply(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
+	fmt.Println("from client side ", req.Msg)
+	return &proto.HelloResponse{
+		Msg: "this is anunj send response from server",
+	}, nil
+}
+
 func main() {
 	listener, tcpErr := net.Listen("tcp", ":9000")
-
 	if tcpErr != nil {
 		log.Fatal(tcpErr)
 	}
 
 	srv := grpc.NewServer()
 	proto.RegisterMessageExampleServer(srv, &server{})
-	log.Printf("server started @ %v", listener)
+
+	log.Printf("server started @ %v", listener.Addr())
+
 	if err := srv.Serve(listener); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func (s *server) ServerReply(ctx context.Context, req *proto.HelloRequest) (*proto.HelloResponse, error) {
-	return &proto.HelloResponse{
-		Msg: "this is anunj send response from server",
-	}, nil
 }
